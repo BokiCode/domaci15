bb051caa809ce875be802ec1fcb4b730f76ea7ff
BokiCode <bojancodedev@gmail.com>
2024-02-05 22:01:42 +0100

    listed users names, emails and avatars

--- js/script.js
+++ js/script.js
@@ -1,61 +1,17 @@
-// $(document).ready(function () {
-//     $.ajax({
-//         url: "https://reqres.in/api/users?page=2",
-//         success: function (response) {
-//             for(let user in response.data) {
-//                 let email = response.data[user]["email"];
-//                 let name = response.data[user]["first_name"];
-//                 $("#userList").append("<p>"+ email + " - " + name +"</p>");
-//             }
-//         }
-//     });
-//     $.ajax({
-//         url: "https://reqres.in/api/users",
-//         type: "POST",
-//         data: {
-//             name: "Toma",
-//             job: "programer",
-//         },
-//         success: function(response) {
-//             console.log(response);
-//         }
-//     });
-//     $("#deleteButton").click(function() {
-//        $.ajax({
-//            url: "https://reqres.in/api/users/2",
-//            type: "DELETE",
-//            success: function(response) {
-//                console.log(response);
-//            }
-//        });
-//     });
-//     $("#registerButton").click(function () {
-//         let newEmail = $("#emailInput").val();
-//         let newName = $("#nameInput").val();
-//        $.ajax({
-//            url: "https://reqres.in/api/users?page=2",
-//            type: "POST",
-//            data: {
-//                name: newName,
-//                email: newEmail
-//            },
-//            success: function(response) {
-//                console.log(response);
-//            }
-//        });
-//     });
-// });
 $(document).ready(function () {
     $.ajax({
         url: "https://reqres.in/api/users?page=2",
         success: function (response) {
-            for (const user in response.data) {
-                const email = response.data[user]["email"];
-                const name = response.data[user]["first_name"];
-                const avatar = response.data[user]["avatar"]
-                $("body").append("<p>" + email + " - " + name +  "</p>");
+            for (const user of response.data) {
+                const image = $("<img>").attr({
+                    "src": user["avatar"],
+                    "alt": user["first_name"] + "'s avatar",
+                });
+                const nameParagraph = $("<p>").html(user["first_name"]);
+                const emailParagraph = $("<p>").html(user["email"]);
+                const userDiv = $("<div>").append(nameParagraph, emailParagraph, image);
+                $("#usersContainer").append(userDiv);
             }
         }
-
     });
 });
